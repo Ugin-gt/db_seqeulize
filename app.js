e53e6b58a571a53d5e0ee167ca771b1da47c62ec
Ugin-gt <apach575@gmail.com>
2021-04-05 15:12:15 +0300

    Copy code from fccourses

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const router = require('./router');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // data stream -> json -> js object -> req.body
+/* 
+  http://localhost:3000/api/*
+ */
+app.use('/api', router);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({
+    errors: [{ message: err.message }],
+  });
+});
 
 module.exports = app;
